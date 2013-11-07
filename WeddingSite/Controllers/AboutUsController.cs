e837cbdbc3522df0c50f1c814da4b7323d396d5d
Mike Schanck <mjschanck@gmail.com>
2013-11-06 23:54:07 -0500

    This is the first commit

---
+++ WeddingSite/Controllers/AboutUsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingSite.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        //
+        // GET: /AboutUs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WeddingSite/Controllers/RegistryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingSite.Controllers
+{
+    public class RegistryController : Controller
+    {
+        //
+        // GET: /Registry/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
