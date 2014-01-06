c417e60886d982ef0208af5e0e184c51fe0b7759
Mike Schanck <mjschanck@gmail.com>
2014-01-06 01:31:06 -0500

    Added Image for Reception, replaced content on homepage, ceremony to reception. Updated css for pages not running underneath navbar

---
+++ WeddingSite/Controllers/ReceptionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingSite.Controllers
+{
+    public class ReceptionController : Controller
+    {
+        //
+        // GET: /Reception/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
